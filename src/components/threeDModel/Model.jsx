8b7db7e56282873f8b97a34ef1ecd83707ca67e5
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2025-07-03 16:34:30 +0530

    df

--- src/components/threeDModel/Model.jsx
+++ src/components/threeDModel/Model.jsx
@@ -9,6 +9,7 @@ function Model() {
             <Spline scene="https://prod.spline.design/Kc0U5W-mcKzGoxtk/scene.splinecode" className={styles.Spliendesign}/>
           </div>
     )
+    
 };
 // https://my.spline.design/untitled-c7fe10b86bd7f6743f8acd38b0abcd58/
 // https://prod.spline.design/f022O34znqOzSb5J/scene.splinecode
